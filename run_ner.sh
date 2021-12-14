7bba47b45be0a4d8fbd2defdbda4f0124863f879
D3auuvc <chenghan.chung@ut.ee>
2021-12-14 22:38:09 +0200

    [Upd] Updated setting in sh file

--- run_ner.sh
+++ run_ner.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:tesla:6
 #SBATCH --job-name=estner
 #SBATCH --mail-type=ALL
 #SBATCH --time 1-00:00:00
@@ -10,10 +10,11 @@
 
 source $HOME/estbert_venv/bin/activate
 
+module load git-lfs/2.11.0
+
 wandb login
 WANDB_PROJECT=EstNewNER
 
 CONFIG_PATH=config/new_ner_config.json
 
 python run_ner.py $CONFIG_PATH
-    
