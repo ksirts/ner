e062745b750c9c32966df506b6829a74158cada2
D3auuvc <chenghan.chung@ut.ee>
2021-12-14 22:56:16 +0200

    [Upd] Updated folder

--- run_ner.py
+++ run_ner.py
@@ -62,7 +62,7 @@ class ModelArguments:
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
     """
 
-    model_name_or_path: str = field(
+    model_name_or_path: Optional[str]  = field(
         metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
     config_name: Optional[str] = field(
