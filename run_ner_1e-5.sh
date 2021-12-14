c6efc8162c639a363e18d44de670efc2717d141b
D3auuvc <chenghan.chung@ut.ee>
2021-12-15 00:13:49 +0200

    [Upd] Updated learning rate 1e-5 sh file

--- run_ner_1e-5.sh
+++ run_ner_1e-5.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=estner_1e-5
+#SBATCH --job-name=ner_1e-5
 #SBATCH --mail-type=ALL
 #SBATCH --time 1-00:00:00
 #SBATCH --mem=20000
@@ -13,6 +13,6 @@ source $HOME/estbert_venv/bin/activate
 wandb login
 WANDB_PROJECT=EstNewNER
 
-CONFIG_PATH=config/new_ner_config.json
+CONFIG_PATH=config/new_ner_config_1e-5.json
 
 python run_ner.py $CONFIG_PATH
