92915cbccf3cd683689b5439452033c7509d6896
D3auuvc <chenghan.chung@ut.ee>
2021-12-16 13:03:53 +0200

    [Upd] Updated formated

--- run_ner_5e-7.sh
+++ run_ner_5e-7.sh
@@ -13,6 +13,6 @@ source $HOME/estbert_venv/bin/activate
 wandb login
 WANDB_PROJECT=EstNewNER
 
-CONFIG_PATH=./config/new_ner_config_5e-7.json
+CONFIG_PATH=config/new_ner_config_5e-7.json
 
 python run_ner.py $CONFIG_PATH
