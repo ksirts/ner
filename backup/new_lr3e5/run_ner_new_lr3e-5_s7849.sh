5a8961e3ab721d648c3070135b7c73ef607e8747
D3auuvc <chenghan.chung@ut.ee>
2022-01-02 10:54:53 +0200

    [Upd] Updated backup files and folders

---
+++ backup/new_lr3e5/run_ner_new_lr3e-5_s7849.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=n35s7849
+#SBATCH --mail-type=ALL
+#SBATCH --time 1-00:00:00
+#SBATCH --mem=20000
+#SBATCH --output=R-%x.%j.out
+
+module load python/3.8.6
+source venv/bin/activate
+
+wandb login
+WANDB_PROJECT=EstNewNER
+
+CONFIG_PATH=config/config_ner_new_lr3e-5_s7849.json
+
+python run_ner_main.py $CONFIG_PATH
