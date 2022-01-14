0c2eaf2eea71074ceeb0bebcb7a8e7636bc7725b
D3auuvc <chenghan.chung@ut.ee>
2022-01-14 16:52:49 +0200

    [Add] Added task original seed number

---
+++ run_ner_main_lr5e-6_s4378.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=m56s4378
+#SBATCH --mail-type=ALL
+#SBATCH --time 1-00:00:00
+#SBATCH --mem=20000
+#SBATCH --output=R-%x.%j.out
+
+module load python/3.8.6
+source venv/bin/activate
+
+wandb login
+WANDB_PROJECT=EstNewNER
+
+CONFIG_PATH=config/config_ner_main_lr5e-6_s4378.json
+
+python run_ner_main.py $CONFIG_PATH
