a7eda8d3187b984724dad4e2f139d1dc01839c01
D3auuvc <chenghan.chung@ut.ee>
2022-02-02 09:09:20 +0200

    [Upd] Updated dataset with remove some enetities label

--- estner/estner.py
+++ estner/estner.py
@@ -38,16 +38,22 @@ The named entity tags use the BIO format, which means that the first word in a n
 # _TEST_FILE = "EstNER_v1_test.json"
 
 # new dataset
-NEW_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_new"
+# NEW_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_new"
 # _TRAINING_FILE = "EstNER_new_train.json"
-_DEV_FILE = "EstNER_new_dev.json"
+# _DEV_FILE = "EstNER_new_dev.json"
 # _TEST_FILE = "EstNER_new_test.json"
 
 # merge new and main dataset
-_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_merg"
-_TRAINING_FILE = "EstNER_merg_train.json"
+# _PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_merg"
+# _TRAINING_FILE = "EstNER_merg_train.json"
 # _DEV_FILE = "EstNER_merg_dev.json"
-_TEST_FILE = "EstNER_merg_test.json"
+# _TEST_FILE = "EstNER_merg_test.json"
+
+# remove some entities lables
+_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_token"
+_TRAINING_FILE = "EstNER_token_train.json"
+_DEV_FILE = "EstNER_token_dev.json"
+_TEST_FILE = "EstNER_token_test.json"
 
 
 class EstNERConfig(datasets.BuilderConfig):
@@ -115,7 +121,7 @@ class EstNER(datasets.GeneratorBasedBuilder):
         """Returns SplitGenerators."""
         file_paths = {
             "train": os.path.join(_PATH, _TRAINING_FILE),
-            "dev": os.path.join(NEW_PATH, _DEV_FILE),
+            "dev": os.path.join(_PATH, _DEV_FILE),
             "test": os.path.join(_PATH, _TEST_FILE),
         }
 
