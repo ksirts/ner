0e8ed8805329c43446508c5aa0d8bc58126d5893
D3auuvc <chenghan.chung@ut.ee>
2022-02-04 04:30:08 +0200

    [Upd] Updated data loading script

--- estner/estner.py
+++ estner/estner.py
@@ -32,16 +32,16 @@ The named entity tags use the BIO format, which means that the first word in a n
 """
 
 # main dataset
-_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_main"
-_TRAINING_FILE = "EstNER_v1_train.json"
-_DEV_FILE = "EstNER_v1_dev.json"
-_TEST_FILE = "EstNER_v1_test.json"
+# _PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_main"
+# _TRAINING_FILE = "EstNER_v1_train.json"
+# _DEV_FILE = "EstNER_v1_dev.json"
+# _TEST_FILE = "EstNER_v1_test.json"
 
 # new dataset
-# NEW_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_new"
-# _TRAINING_FILE = "EstNER_new_train.json"
-# _DEV_FILE = "EstNER_new_dev.json"
-# _TEST_FILE = "EstNER_new_test.json"
+_PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_new"
+_TRAINING_FILE = "EstNER_new_train.json"
+_DEV_FILE = "EstNER_new_dev.json"
+_TEST_FILE = "EstNER_new_test.json"
 
 # merge new and main dataset
 # _PATH = "/gpfs/space/home/chenghan/ner/data/EstNER_merg"
